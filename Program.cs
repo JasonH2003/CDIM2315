eb55449449283d5ef0c14c140803214fdc7facff
JasonH2003 <112124117+JasonH2003@users.noreply.github.com>
2022-08-25 15:32:08 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HW_1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double X = 2.5;
+        double Y = 3.3;
+        
+        double Z = Convert.ToDouble(( 4 * (X * X)) + (3*Y));
+
+        Console.WriteLine($"X = {X}, Y = {Y}");
+        Console.WriteLine($"The value of Z is: {Z}");
+    }
+}
