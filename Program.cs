221bf552697cf1d0a6c385ebb38d3a149d9e99c7
JasonH2003 <112124117+JasonH2003@users.noreply.github.com>
2022-09-07 21:13:08 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace  HW_2;
+class Program
+{
+    static void Main(string[] args)
+    {   
+        //Q1
+         Console.WriteLine("Input Letter Grade: ");
+       string grade = Console.ReadLine();
+       Convert.ToChar(grade);
+       if (grade=="A"){
+        Console.WriteLine("Grade Point: 4");
+       }
+       else if (grade=="B"){
+        Console.WriteLine("Grade Point: 3");
+       }
+        else if (grade=="C"){
+        Console.WriteLine("Grade Point: 2");
+       }
+        else if (grade=="D"){
+        Console.WriteLine("Grade Point: 1");
+       }
+         else if (grade=="F"){
+        Console.WriteLine("Grade Point: 0");
+       }
+        else{
+            Console.WriteLine("Wrong Letter Grade!");
+        }
+       // Q2
+
+        Console.WriteLine("Input the first number: ");
+        string N1 = Console.ReadLine();
+        int n1 = Convert.ToInt32(N1);
+        Console.WriteLine("Input the second number: ");
+        string N2 = Console.ReadLine();
+         int n2 = Convert.ToInt32(N2);
+        Console.WriteLine("Input the third number: ");
+        string N3 = Console.ReadLine();
+        int n3 = Convert.ToInt32(N3);
+
+        if(n1 < n2 && n1 < n3){
+            Console.WriteLine($"The smallest value is: {n1} ");
+        }
+        else if(n2 < n1 && n2 < n3){
+            Console.WriteLine($"The smallest value is: {n2} ");
+        }
+       else if(n3 < n1 && n3 < n2){
+            Console.WriteLine($"The smallest value is: {n3} ");
+        }
+
+   
+}
+}
